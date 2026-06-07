245d9b966143fca9ab7d70c2cb70517d143b85e3
Claudio Bottino <s359291@studenti.polito.it>
2026-06-07 22:36:46 +0200

    Add user authentication API and integrate with App component; implement feedback context

---
+++ client/src/API.js
@@ -0,0 +1,52 @@
+
+const SERVER_URL = 'http://localhost:3001/api';
+
+
+/**
+ * This function wants username and password inside a "credentials" object.
+ * It executes the log-in.
+ */
+const logIn = async (credentials) => {
+    return await fetch(SERVER_URL + '/sessions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',  // this parameter specifies that authentication cookie must be forwared. It is included in all the authenticated APIs.
+        body: JSON.stringify(credentials),
+    }).then(handleInvalidResponse)
+    .then(response => response.json());
+};
+  
+/**
+ * This function is used to verify if the user is still logged-in.
+ * It returns a JSON object with the user info.
+ */
+const getUserInfo = async () => {
+    return await fetch(SERVER_URL + '/sessions/current', {
+        credentials: 'include'
+    }).then(handleInvalidResponse)
+    .then(response => response.json());
+};
+
+/**
+ * This function destroy the current user's session (executing the log-out).
+ */
+const logOut = async() => {
+  return await fetch(SERVER_URL + '/sessions/current', {
+    method: 'DELETE',
+    credentials: 'include'
+  }).then(handleInvalidResponse);
+}
+
+function handleInvalidResponse(response) {
+    if (!response.ok) { throw Error(response.statusText) }
+    let type = response.headers.get('Content-Type');
+    if (type !== null && type.indexOf('application/json') === -1){
+        throw new TypeError(`Expected JSON, got ${type}`)
+    }
+    return response;
+}
+
+const API = {logIn, getUserInfo, logOut};
+export default API;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,121 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css'
+
+import { useEffect, useState } from 'react';
+
+import API from "./API.js";
+import FeedbackContext from "./contexts/FeedbackContext.js";
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const [user, setUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [feedback, setFeedback] = useState('');
+  const [shouldRefresh, setShouldRefresh] = useState(true);
+  
+
+  const setFeedbackFromError = (err) => {
+    let message = '';
+    if (err.message) message = err.message;
+    else message = "Unknown Error";
+    setFeedback(message); // Assuming only one error message at a time
+  };
+
+  useEffect(() => {
+      // Checking if the user is already logged-in
+      // This useEffect is called only the first time the component is mounted (i.e., when the page is (re)loaded.)
+      API.getUserInfo()
+        .then(user => {
+          setLoggedIn(true);
+          setUser(user);  // here you have the user info, if already logged in
+        }).catch(e => {
+          if(loggedIn)    // printing error only if the state is inconsistent (i.e., the app was configured to be logged-in)
+            setFeedbackFromError(e);
+          setLoggedIn(false); setUser(null);
+        }); 
+  }, []);
+
+  /**
+   * This function handles the logout process.
+   */ 
+  const handleLogout = async () => {
+      await API.logOut();
+      // clean up everything
+      setLoggedIn(false); setUser(null);
+  };
+
+  const handleLogin = async (credentials) => {
+    const user = await API.logIn(credentials);
+    setUser(user); setLoggedIn(true);
+    setFeedback("Welcome, "+user.name);
+  };
+
+
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <FeedbackContext.Provider value={{setFeedback, setFeedbackFromError, setShouldRefresh}}>
+      <div className="min-vh-100 d-flex flex-column">
+        <Header logout={handleLogout} user={user} loggedIn={loggedIn} />
+      </div>
+    </FeedbackContext.Provider>
   )
 }
 
